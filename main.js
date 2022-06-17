e06f2bc8e6f7f615ec76b32a0d86d32205309f62
jiale3945 <jiale3945@gmail.com>
2022-06-17 10:10:31 +0800

    v1

---
+++ main.js
@@ -0,0 +1,35 @@
+import *as THREE from 'three'
+
+const scene = new THREE.Scene()
+const camera = new THREE.
+    PerspectiveCamera(
+        75,
+        innerWidth / innerHeight,
+        0.1,
+        1000
+    )
+
+    const renderer = new THREE.WebGLrenderer(
+    )
+    renderer.setSize(innerWidth,innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    //create a sphere
+    const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50),
+    new THREE.MeshBasicMaterial({
+        // color: 0xff0000
+        map:new THREE.TextureLoader().load('
+        ./IMG/01-3.jpg')
+    })
+    )
+
+    scene.add(sphere)
+
+    camera.position.z=10
+
+    function animate(){
+        requestAnimationFrame(animate)
+        renderer.render(scene,camera)
+    }
+    animate()
+
